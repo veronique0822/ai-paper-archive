4534d56f49b34009b631b786bee0504c8adc0409
veronique0822 <262276317@qq.com>
2026-09-11 21:17:07 +0800

    init: 添加 data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+// 档案馆索引数据。每期新内容发布后，在 entries 数组头部插入一条记录。
+// 字段：type(week|day|learn)、date(YYYY-MM-DD)、title、url、summary、tags[]、companies[]、count
+window.ARCHIVE_DATA = {
+  "updated": "2026-09-11",
+  "entries": [
+  ]
+};
